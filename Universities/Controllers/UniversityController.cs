c26929e378e368c69a9aea550f35013c751e3dfc
William Gleeson <wgleeson157@gmail.com>
2020-07-16 12:28:49 -0500

    Initial code commit

---
+++ Universities/Controllers/UniversityController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using Universities.Models;
+
+namespace Universities.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UniversityController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<List<University>> Get()
+        {
+            var client = new RestClient("http://universities.hipolabs.com/search");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = await client.ExecuteGetAsync(request);
+
+            if (!response.IsSuccessful)
+            {
+                string message = "Error making request.  Status code: " + response.StatusCode;
+                var exception = new ApplicationException(message);
+                throw exception;
+            }
+
+            List<University> universityList = (List<University>)JsonConvert.DeserializeObject(response.Content, typeof(List<University>));
+            universityList.Sort((x, y) => string.Compare(x.Name, y.Name));
+
+            return universityList;
+        }
+    }
+}
---
+++ Universities/Models/University.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Universities.Models
+{
+    public class University
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("web_pages")]
+        public List<string> Web { get; set; }
+    }
+}
